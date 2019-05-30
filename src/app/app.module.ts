ebbbb18acc9db0c2dc1ae75a826a205e25afec67
wagner <wagnerricardonet@gmail.com>
2019-05-30 08:23:53 -0300

    orders form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule, LOCALE_ID } from "@angular/core";
 import { HttpModule } from "@angular/http";
 import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 import { ROUTES } from "./app.routes";
 
@@ -18,6 +19,7 @@ import { ShoppingCartComponent } from "./restaurant-detail/shopping-cart/shoppin
 import { MenuItemComponent } from "./restaurant-detail/menu-item/menu-item.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
 import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
+import { OrderComponent } from "./order/order.component";
 
 @NgModule({
   declarations: [
@@ -31,10 +33,20 @@ import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-
     MenuComponent,
     ShoppingCartComponent,
     MenuItemComponent,
-    ReviewsComponent
+    ReviewsComponent,
+    OrderComponent
   ],
-  imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
-  providers: [RestaurantsService, ShoppingCartService],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(ROUTES),
+    FormsModule
+  ],
+  providers: [
+    RestaurantsService,
+    ShoppingCartService,
+    { provide: LOCALE_ID, useValue: "pt-BR" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
